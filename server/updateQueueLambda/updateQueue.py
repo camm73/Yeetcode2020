7ae74238d604156e2ac7ccd878bf1d38ca8ebd43
Cameron Miller <camm73@yahoo.com>
2020-11-14 15:01:05 -0800

    Remove song after chosen song has been used to update the queue

--- server/updateQueueLambda/updateQueue.py
+++ server/updateQueueLambda/updateQueue.py
@@ -2,6 +2,7 @@ import boto3
 import json
 
 dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+apiMgmt = boto3.client('apigatewaymanagementapi', region_name='us-east-1', endpoint_url='https://8mvqn1b54i.execute-api.us-east-1.amazonaws.com/production/')
 TABLE = 'Yeetcode2020-Data'
 
 def lambda_handler(event, context):
@@ -46,8 +47,57 @@ def lambda_handler(event, context):
     leaderboard = res['Item']['leaderboard']['M']
 
     # TODO: Determine which song in leaderboard is the lowest
+    
+    songURI = 'here'  # TODO: CHANGE WHEN DONE TESTING
+
+    # ========================================================
+
+    # Remove chosen song from leaderboard
+    try:
+        update_res = dynamodb.update_item(
+            TableName=TABLE,
+            Key={
+                'partyID': {
+                    'S': partyID
+                }
+            },
+            ExpressionAttributeNames={
+                "#uri": songURI
+            },
+            UpdateExpression="REMOVE leaderboard.#uri",
+            ReturnValues='ALL_NEW'
+        )
+        print(update_res)
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "body": "Failed to remove song from leaderboard"
+        }
+
+    # Get newly updated data
+    updated_data = update_res['Attributes']
+    leaderboard = updated_data['leaderboard']['M']
+    clients = updated_data['clients']['L']
+
+    ret_packet = {
+        "action": "leaderboardUpdate",
+        "leaderboard": leaderboard
+    }
+
+    # Send to clients
+    for entry in clients:
+        connID = entry['S']
+        send_to_client(ret_packet, connID)
 
     return {
         'statusCode': 200,
         'body': "DONE"
     }
+
+def send_to_client(data, connectionId):
+    # Send leaderboard to user
+    apiMgmt.post_to_connection(
+        Data=json.dumps(data),
+        ConnectionId=connectionId
+    )
