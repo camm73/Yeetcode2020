9682f9668a8901bdcd8004985c9ebc2dd30e4301
Andy Cai <andycai20000212@gmail.com>
2020-11-14 14:24:53 -0800

    feeling like a failure

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Home } from './views/Home';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from './views/Home';
+import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
+import SignIn from './views/SignIn';
 
 export const Routes = () => {
 	return (
 		<div>
-			<NavBar />
-			<Switch>
-				<Route exact path="/Home" component={Home} />
-				<Route exact path="/">
-					<Redirect to="/Home" />
-				</Route>
-				<Route exact path="/About" component={About} />
-			</Switch>
+			<Router>
+				<Switch>
+					<Route exact path="/Home" component={Home} />
+					<Route exact path="/">
+						<Redirect to="/Home" />
+					</Route>
+					<Route exact path="/signin" component={SignIn} />
+				</Switch>
+			</Router>
 		</div>
 	);
 };
