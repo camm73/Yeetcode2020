7a559602150e2c5ae46150c4122534a50f53c4e4
Cameron Miller <camm73@yahoo.com>
2020-11-15 07:00:14 +0000

    Updated publish script to handle libraries

--- server/updateQueueLambda/publish.sh
+++ server/updateQueueLambda/publish.sh
@@ -2,6 +2,10 @@
 
 rm updateQueue.zip
 zip -r9 updateQueue.zip *.py
+
+cd packages/
+zip -ur ../updateQueue.zip *
+cd ..
 echo "File successfully zipped"
 
 aws lambda update-function-code --function-name yeetcode2020-updateQueue --zip-file fileb://updateQueue.zip
